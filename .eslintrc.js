f588fbedc744ef0930e0f8cf39ca4c1e465f7cf9
Ivan Martinez <ivanthegreat15@gmail.com>
2018-12-11 19:49:00 -0500

    Fix duplicates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
     "extends": "airbnb-base"
+    rules: {
+        "linebreak-style": 0,
+    }
 };
--- index.js
+++ index.js
@@ -8,83 +8,101 @@ let issues;
 let oldIssues;
 let lastFetch;
 
+const titles = [];
+
 const API_URL = 'https://gitcoin.co/api/v0.1/bounties?is_open=true';
 
 const createEmbed = (issue) => {
   const expire = new Date(issue.expires_date);
   const reward = `${issue.value_true} ${issue.token_name} ${
-    issue.value_in_usdt ? `($${issue.value_in_usdt})` : ''
-  }`;
+  issue.value_in_usdt
+    ? `($${issue.value_in_usdt})`
+    : ''}`;
 
   const fields = [];
   if (issue.experience_level) {
-    fields.push({
-      name: 'Difficulty',
-      value: issue.experience_level,
-    });
+    fields.push({name: 'Difficulty', value: issue.experience_level});
   }
-  fields.push(
-    {
-      name: 'Keywords',
-      value: issue.keywords,
-    },
-    {
-      name: 'Reward',
-      value: reward,
-    },
-    {
-      name: 'Expires',
-      value: `${expire.getMonth()}/${expire.getDate()}/${expire.getFullYear()}${
-        issue.can_submit_after_expiration_date ? ' (Can submit after expiration)' : ''
-      }`,
-    },
-  );
+  fields.push({
+    name: 'Keywords',
+    value: issue.keywords
+  }, {
+    name: 'Reward',
+    value: reward
+  }, {
+    name: 'Expires',
+    value: `${expire.getMonth()}/${expire.getDate()}/${expire.getFullYear()}${issue.can_submit_after_expiration_date
+      ? ' (Can submit after expiration)'
+      : ''}`
+  });
 
   return {
     embed: {
       color: 2418840,
       author: {
         name: client.user.username,
-        icon_url: client.user.avatarURL,
+        icon_url: client.user.avatarURL
       },
       title: issue.title,
       url: issue.url,
       fields,
       footer: {
         icon_url: client.user.avatarURL,
-        text: 'Powered by Gitcoin',
-      },
-    },
+        text: 'Powered by Gitcoin'
+      }
+    }
   };
 };
 
 // TODO rewrite so it checks the message history and doesn't post duplicates
 const getNewIssues = () => {
-  axios.get(API_URL).then((result) => {
-    issues = Object.values(result.data);
-    issues = issues.slice(issues.length - 16).filter(issue => issue.network === 'mainnet');
-    const newIssues = issues.filter(issue => !oldIssues.includes(issue));
-    console.log(newIssues);
-    const gitcoinChannel = client.channels.get(process.env.GITCOIN_CHANNEL);
-    issues.forEach((issue) => {
-      console.log(createEmbed(issue));
-      gitcoinChannel.send(createEmbed(issue));
-    });
-    oldIssues = issues;
-    return true;
-  });
-};
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
   axios
     .get(API_URL)
     .then((result) => {
       issues = Object.values(result.data);
-      issues = issues.slice(issues.length - 16).filter(issue => issue.network === 'mainnet');
-      const channel = client.channels.get(process.env.GITCOIN_CHANNEL);
-      console.log(issues[0]);
-      issues.forEach(issue => channel.send(createEmbed(issue)));
+      issues = issues
+        .slice(issues.length - 16)
+        .filter(issue => issue.network === 'mainnet')
+        .filter(issue => !titles.includes(issue.title));
+      const newIssues = issues.filter(issue => !oldIssues.includes(issue));
+      console.log("Pushed new issues");
+      const gitcoinChannel = client
+        .channels
+        .get(process.env.GITCOIN_CHANNEL);
+      issues.forEach((issue) => {
+        console.log(createEmbed(issue));
+        gitcoinChannel.send(createEmbed(issue));
+      });
+      oldIssues = issues;
+      return true;
+    });
+};
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  const gitcoinChannel = client
+    .channels
+    .get(process.env.GITCOIN_CHANNEL);
+  gitcoinChannel
+    .fetchMessages()
+    .then(msg => {
+      const ids = msg.map(msg => msg.id);
+      ids.forEach(id => gitcoinChannel.fetchMessage(id).then(msg => titles.push(msg.embeds[0].title)))
+    })
+    .catch(console.error);
+  axios
+    .get(API_URL)
+    .then((result) => {
+      issues = Object.values(result.data);
+      issues = issues
+        .slice(issues.length - 16)
+        .filter(issue => issue.network === 'mainnet');
+      const channel = client
+        .channels
+        .get(process.env.GITCOIN_CHANNEL);
+      issues
+        .filter(issue => !titles.includes(issue.title))
+        .forEach(issue => channel.send(createEmbed(issue)));
       oldIssues = issues;
       client.setInterval(getNewIssues, 20 * (1000 * 60));
       return true;
